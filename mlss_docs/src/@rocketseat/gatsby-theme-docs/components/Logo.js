2518abaeed04f9324b1ed307e1f1a447bcd01ef7
Florian Georg <florian.alexander.georg@gmail.com>
2022-07-04 13:37:43 +0200

    changed withPrefix for loading image

--- mlss_docs/src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ mlss_docs/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { withPrefix } from "gatsby";
 
-// Path to the logo file on your project
+// Path to the logo file on your project (we need withPrefix to get the correct path for deployed sites)
 import logo from "/static/banner.png";
 
-const Logo = () => <img src={logo} alt="Logo" style={{ width: 90 }} />;
+const Logo = () => (
+  <img src={withPrefix(logo)} alt="Logo" style={{ width: 90 }} />
+);
 
 export default Logo;

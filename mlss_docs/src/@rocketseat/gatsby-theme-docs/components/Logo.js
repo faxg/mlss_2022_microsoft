8f93f7ba6bc93f8cc6e965d1c1c9b769ee31c09c
Florian Georg <florian.alexander.georg@gmail.com>
2022-07-04 11:24:54 +0200

    first draft version

---
+++ mlss_docs/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// Path to the logo file on your project
+import logo from "/static/banner.png";
+
+const Logo = () => <img src={logo} alt="Logo" style={{ width: 180 }} />;
+
+export default Logo;
